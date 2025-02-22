5a8ad7a9c0948970c3ec1c6bffcac131bc1f824e
sreelekhasreenivasan123@gmail.com <sreelekhasreenivasan123@gmail.com>
2025-02-22 14:11:39 +0530

    updated tabs

--- travelblog/app/(tabs)/_layout.tsx
+++ travelblog/app/(tabs)/_layout.tsx
@@ -2,39 +2,25 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
 import { FontAwesome, Ionicons, MaterialIcons } from '@expo/vector-icons'
+import Colors from '@/constants/Colors'
 
 export default function _layout() {
   return (
-    <Tabs>
+    <Tabs screenOptions={{
+      tabBarStyle:{
+        backgroundColor: Colors.bgColor,
+        borderTopWidth:0,
+        padding:0
+      },
+      tabBarShowLabel:false,
+      tabBarActiveTintColor:Colors.black,
+      tabBarInactiveTintColor:'#999'
+    }}>
       <Tabs.Screen 
         name='index' 
         options={{ 
           tabBarIcon: ({ color }) => (
             <Ionicons name='compass' size={28} color={color} />
-          )
-        }}
-      />
-      <Tabs.Screen 
-        name='bookmark' 
-        options={{ 
-          tabBarIcon: ({ color }) => (
-            <Ionicons name='bookmark' size={28} color={color} />
-          )
-        }}
-      />
-      <Tabs.Screen 
-        name='search' 
-        options={{ 
-          tabBarIcon: ({ color }) => (
-            <Ionicons name='search' size={28} color={color} />
-          )
-        }}
-      />
-      <Tabs.Screen 
-        name='profile' 
-        options={{ 
-          tabBarIcon: ({ color }) => (
-            <FontAwesome name='user' size={28} color={color} />
           )
         }}
       />
@@ -46,6 +32,42 @@ export default function _layout() {
           )
         }}
       />
+      <Tabs.Screen 
+        name='search' 
+        options={{ 
+          tabBarIcon: ({ color }) => (
+            <View style={{
+              backgroundColor:Colors.primaryColor,
+              height:46,
+              width:45,
+              paddingHorizontal:10,
+              paddingVertical:10,
+              borderRadius:12
+            }}>
+            <Ionicons name='search-outline' size={24} color={Colors.white} />
+            </View>
+          )
+        }}
+      />
+
+      <Tabs.Screen 
+        name='bookmark' 
+        options={{ 
+          tabBarIcon: ({ color }) => (
+            <Ionicons name='bookmark' size={28} color={color} />
+          )
+        }}
+      />
+
+      <Tabs.Screen 
+        name='profile' 
+        options={{ 
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name='user' size={28} color={color} />
+          )
+        }}
+      />
+      
     </Tabs>
   )
 }
--- travelblog/app/(tabs)/index.tsx
+++ travelblog/app/(tabs)/index.tsx
@@ -1,11 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native'
+import {Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { Stack } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'
+import Colors from '@/constants/Colors'
 
 const Page = () => {
   return (
-    <View style={styles.container}>
-      <Text>Page</Text>
-    </View>
+    <Stack.Screen options={{
+      headerTransparent:true,
+      headerTitle:"",
+      headerLeft:()=>(
+        <TouchableOpacity onPress={()=>{}} style={{marginLeft:20}}>
+          <Image
+            source={{
+              uri:"https://xsgames.co/randomusers/avatar.php?g=female"
+            }}
+            style={{width:40,height:40,borderRadius:10}}
+          />
+        </TouchableOpacity>
+      ),
+      headerRight:()=>(
+        <TouchableOpacity onPress={()=>{}} style={{
+          marginRight:20,
+          backgroundColor:Colors.white,
+          padding:10,
+          borderRadius:10,
+          shadowColor:"#171717",
+          shadowOffset:{width:2, height:4},
+          shadowOpacity:0.2,
+          shadowRadius:3,
+        }}>
+          <Ionicons name='notifications' size={30} color={Colors.black}/>
+        </TouchableOpacity>
+      )
+
+    }}/>
+
+   
   )
 }
 
